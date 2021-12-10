782149781423d910ba6e95d0bb1edb34fd0b1d75
BelskiyJS <87985970+BelskiyJS@users.noreply.github.com>
2021-12-10 12:27:21 +0300

    Delete MathMat/js directory

--- MathMat/js/app.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",()=>{"serviceWorker"in navigator&&navigator.serviceWorker.register("./sw.js").then(e=>{console.log("Service worker successfully registered",e)}).catch(e=>{console.log("Service worker registration failed",e)})});
